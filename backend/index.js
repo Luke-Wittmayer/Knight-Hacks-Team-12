1943eef91e384f338b1303c3667567e2e2a957b3
SouloCider <luke.wittmayer@knights.ucf.edu>
2022-03-20 17:38:02 -0400

    Update index.js
    add reference to data collection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import app from "./server.js"
 import mongodb from "mongodb"
 import dotenv from "dotenv"
+import internshipsDAO from "./dao/internshipsDAO.js";
+
 dotenv.config()
 const MongoClient = mongodb.MongoClient;
 
@@ -19,6 +21,7 @@ MongoClient.connect(process.env.INTERNTRACKER_DB_URL,
     process.exit(1)
 })
 .then(async client => {
+    await internshipsDAO.injectDB(client)
     app.listen(port, () => {
         console.log(`listening to port ${port}`)
     })
