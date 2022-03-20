b80d156208f62995a8fd5e992c4eaa2be6e8bef3
SouloCider <luke.wittmayer@knights.ucf.edu>
2022-03-20 16:31:51 -0400

    Update internships.route.js
    adds url routes to sign up and log in users. NOT CURRENTLY WORKING, unable to find bcrypt package.

--- backend/api/internships.route.js
+++ backend/api/internships.route.js
@@ -1,7 +1,14 @@
 import express from "express"
+import UserAuth from "./userAuthentication.js"
 
 const router = express.Router()
 
 router.route("/").get((req,res) => res.send("hello world"))
+
+// Route to sign up. Possibly change to a POST request.
+router.route("/signup").get(UserAuth.signUp);
+
+// Route to log in
+router.route("/login").get(UserAuth.logIn);
 
 export default router
