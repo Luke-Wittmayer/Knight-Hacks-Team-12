2f18c6b94b78459133ab4c8733f177609e9c97d4
SouloCider <luke.wittmayer@knights.ucf.edu>
2022-04-04 19:35:08 -0400

    Creates Welcome Screen
    Edits and creates a base welcome screen. Function within this screen like linking to a different page is not yet implemented.

--- frontend/app/screens/WelcomeScreen.js
+++ frontend/app/screens/WelcomeScreen.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Image, ImageBackground, StyleSheet, View, Text } from 'react-native';
+import App from '../../App';
+
+import AppButton from '../components/AppButton';
+import AppText from '../components/AppText';
+import colors from '../config/colors';
 
 function WelcomeScreen(props) {
     return (
         <ImageBackground 
+            blurRadius={10}
             style={styles.background}
             source={require("../assets/banner.jpg")}
         >
             <View style={styles.logoContainer}>
                 <Image style={styles.logo} source={require('../assets/logo.png')}/>
-                <Text>Internship Tracker</Text>
+                <Text style={styles.title}>Internship Tracker</Text>
             </View>
-            <View style={styles.loginButton}></View>
-            <View style={styles.registerButton}></View>
+            <View style={styles.buttonsContainer}>
+                <AppButton title="Login" />
+                <AppButton title="Register" color="secondary" />
+            </View>
         </ImageBackground>
     );
 }
@@ -23,11 +31,6 @@ const styles = StyleSheet.create({
         justifyContent: "flex-end",
         alignItems: "center",
     },
-    loginButton: {
-        width: "100%",
-        height: 70,
-        backgroundColor: "#fc5c65",
-    },
     logo:{
         width: 170,
         height: 170,
@@ -37,10 +40,15 @@ const styles = StyleSheet.create({
         top: 150,
         alignItems: "center",
     },
-    registerButton: {
-        width: "100%",
-        height: 70,
-        backgroundColor: "#4ecdc4",
+    buttonsContainer: {
+        padding: 20,
+        width: '100%',
+    },
+    title:{
+        fontFamily: 'Courier',
+        fontSize: 25,
+        fontWeight: "600",
+        paddingVertical: 20,
     },
 });
 
