0ebd6d87f8236f44837159070afbb9cd90797fce
Michael Harris <45339433+harrismichael2095@users.noreply.github.com>
2022-04-19 20:37:29 -0400

    connected to the backed

--- frontend/app/screens/LoginScreen.js
+++ frontend/app/screens/LoginScreen.js
@@ -11,13 +11,64 @@ const validationSchema = Yup.object().shape({
 })
 
 function LoginScreen(props) {
+    
+    
+    
+    const loginHandle = (userName, password) => {
+    //console.log(userName)
+    //console.log(password)
+      async function login(){
+        var obj = {username:userName,password:password};
+        var js=JSON.stringify(obj);
+        //console.log(js);
+        try{
+          let response;
+  
+          response = await fetch("http://localhost:3000/api/v1/internships/login",{
+            method: "POST",
+            headers: {"Content-Type" : "application/json"},
+            body : js,
+          });
+        
+
+          var res=JSON.parse(await response.text() );
+          if (res.id<0){
+            console.log(res.errors);
+            
+          }
+          else{
+            var user = {
+              user: res.username,
+            };
+            
+            console.log(user.user)
+            console.log("It worked if you see this message and the username below);
+            store = user.user
+            console.log(store)
+            
+            if(res.id == -1)
+            alert("incorrect username or password")
+          }
+            console.log(res)
+            console.log(res.id)
+        } catch (e){
+          //alert(e.toString());
+          return ;
+        }
+      }
+        //test
+      login();
+   ;
+    //handleMessage(null);
+  };
+    
     return (
         <Screen style={styles.container}>
             <Image style={styles.logo} source={require("../assets/logo.png")}/>
 
             <AppForm
                 initialValues={{ username: '', password: ''}}
-                onSubmit={values => console.log(values)}
+                 onSubmit={values => loginHandle(values.username, values.password)}
                 validationSchema={validationSchema}
             >
                 <AppFormField
